109275307cfdb65538cc7a182cad025ab1ad5a53
Ed <sinyakov.gsg@gmail.com>
2019-02-20 11:47:22 +0300

    Initial commit

---
+++ GitDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitDemo
+//
+//  Created by Eduard Sinyakov on 20/02/2019.
+//  Copyright © 2019 Eduard Sinyakov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
